a65b8f45f32ec318046ca090f7bd4d3cddd10241
Aleksandralucz <luczynska.aleksandraa@gmail.com>
2021-09-22 11:41:10 +0200

    Room Availability

---
+++ src/main/java/HotelManagement/RoomAvailability.java
@@ -0,0 +1,10 @@
+package HotelManagement;
+
+public class RoomAvailability  extends Exception {
+    @Override
+    public String toString()
+    {
+        return "Not Available !";
+    }
+}
+
---
+++ src/main/java/HotelManagement/RoomStandard.java
@@ -0,0 +1,10 @@
+package HotelManagement;
+
+public class RoomStandard {
+
+    Doubleroom luxury_doubleroom[]=new Doubleroom[10];
+    Doubleroom deluxe_doubleroom[]=new Doubleroom[20];
+    Singleroom luxury_singleroom[]=new Singleroom[10];
+    Singleroom deluxe_singleroom[]=new Singleroom[20];
+}
+
